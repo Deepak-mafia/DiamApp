7b15e8364aab9541339982c1642e9c1249216d51
Sitesh Kumar Raulo <sitesh.kumar@blockstack.tech>
2024-10-24 09:42:36 +0530

    Added telegram user data fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [userData, setUserData] = useState(null);
 
+  useEffect(() => {
+    const tg = window.Telegram.WebApp;
+    tg.ready();
+
+    const initDataUnsafe = tg.initDataUnsafe;
+    if (initDataUnsafe.user) {
+      setUserData({
+        id: initDataUnsafe.user.id,
+        username: initDataUnsafe.user.username,
+        firstName: initDataUnsafe.user.first_name,
+        lastName: initDataUnsafe.user.last_name,
+      });
+    }
+  }, []);
   return (
     <>
       <div>
@@ -25,6 +40,18 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+      <div>
+      {userData ? (
+        <div>
+          <p>User ID: {userData.id}</p>
+          <p>Username: {userData.username}</p>
+          <p>First Name: {userData.firstName}</p>
+          <p>Last Name: {userData.lastName}</p>
+        </div>
+      ) : (
+        <p>Loading user data...</p>
+      )}
+    </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
