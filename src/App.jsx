c6afdb9e1d5c49fe4880bf50bb605c0816248f52
Deepak Chaudhary <cdeepak521@gmail.com>
2025-01-06 11:13:59 +0530

    commented icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const [userData, setUserData] = useState(null);
 
   useEffect(() => {
@@ -23,14 +23,14 @@ function App() {
   }, []);
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
+      </div> */}
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -41,29 +41,29 @@ function App() {
         </p> */}
       </div>
       <div>
-      {userData ? (
-        // <div>
-        //   <p>User ID: {userData.id}</p>
-        //   <p>Username: {userData.username}</p>
-        //   <p>First Name: {userData.firstName}</p>
-        //   <p>Last Name: {userData.lastName}</p>
-        // </div>
-        <div>
-        <p>Hello {userData.firstName}! Welcome to the Dummy project.</p>
-        <p>This is your fetch Data of Telegram: </p>
-        <p>User Id: {userData.id}</p>
-        <p>Username: {userData.username}</p>
-        <p>Last Name: {userData.lastName}</p>
+        {userData ? (
+          // <div>
+          //   <p>User ID: {userData.id}</p>
+          //   <p>Username: {userData.username}</p>
+          //   <p>First Name: {userData.firstName}</p>
+          //   <p>Last Name: {userData.lastName}</p>
+          // </div>
+          <div>
+            <p>Hello {userData.firstName}! Welcome to the Dummy project.</p>
+            <p>This is your fetch Data of Telegram: </p>
+            <p>User Id: {userData.id}</p>
+            <p>Username: {userData.username}</p>
+            <p>Last Name: {userData.lastName}</p>
+          </div>
+        ) : (
+          <p>Loading user data...</p>
+        )}
       </div>
-      ) : (
-        <p>Loading user data...</p>
-      )}
-    </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
