3d7b30776be3acca60760102f8b3154cd1b9656c
Deepak Chaudhary <cdeepak521@gmail.com>
2025-01-06 11:24:10 +0530

    commented header card

--- src/App.jsx
+++ src/App.jsx
@@ -32,14 +32,14 @@ function App() {
         </a>
       </div> */}
       {/* <h1>Vite + React</h1> */}
-      <div className="card">
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        {/* <p>
+        <p>
           Edit <code>src/App.jsx</code> and save to test HMR
-        </p> */}
-      </div>
+        </p>
+      </div> */}
       <div>
         {userData ? (
           // <div>
